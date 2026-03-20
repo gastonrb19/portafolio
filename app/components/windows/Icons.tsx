4259f7677eb1fd4ff9803130596cbb8456a06111
gaston rojas <gastonrb19@gmail.com>
2026-03-20 02:10:15 -0300

    cambio en los archivos publicos

--- app/components/windows/Icons.tsx
+++ app/components/windows/Icons.tsx
@@ -2,7 +2,7 @@ import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 
 interface PropsIcons {
-  image : StaticImageData
+  image: StaticImageData;
   link_redirection: string;
 }
 
--- app/components/windows/WrapIcons.tsx
+++ app/components/windows/WrapIcons.tsx
@@ -1,15 +1,11 @@
 import Icons from "./Icons";
-import linkedin from "@/public/linkedin.png"
-import github from "@/public/github.png"
-
+import linkedin from "@/public/linkedin.png";
+import github from "@/public/github.png";
 
 export default function WrapIcons() {
   return (
     <>
-      <Icons
-        link_redirection="https://github.com/gastonrb19/"
-        image={github}
-      />
+      <Icons link_redirection="https://github.com/gastonrb19/" image={github} />
       <Icons
         link_redirection="https://linkedin.com/in/oscar-gastón-rojas-barra-103b36386"
         image={linkedin}
