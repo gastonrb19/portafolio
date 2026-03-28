207c5672569bc0b09ea524174a0b04fc75c3b042
gaston rojas <gastonrb19@gmail.com>
2026-03-28 20:54:49 -0300

    folder component changed import

--- app/components/windows/desktop/folders/Folder.tsx
+++ app/components/windows/desktop/folders/Folder.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { WrapFolderProps } from "./interfaces/Interfaces";
+import { WrapFolderProps } from "../../interfaces/Interfaces";
 import { StaticImageData } from "next/image";
 
 export default function Folder({
